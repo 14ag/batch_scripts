eda6945adeb24832b1e3a0eecfacb71981647403
14ag <muriukipn@gmail.com>
2025-03-05 22:01:13 +0300

    gdrive_download.bat added input sanitization

--- gdrive_download.bat
+++ gdrive_download.bat
@@ -1,6 +1,13 @@
 @echo off
 :: install gdrive, create shortcut of shared folder in yr drive, open it using windows explorer, get shared path, use tis tool to download todo 
-set /p sauce="sauce: "
-set /p de="dest: "
+:main
+set /p "sauce=sauce: "
+set /p "de=dest: "
+
+for %%i in ("%sauce:"=%") do set "sauce=%%~i"
+for %%i in ("%de:"=%") do set "de=%%~i"
+
 robocopy "%sauce%" "%de%" /e /w:2 /njh
+
 pause
+goto :main
