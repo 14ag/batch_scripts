6326631d6f29e30efd0a65870197d448566a00d6
14ag <muriukipn@gmail.com>
2025-03-24 21:21:24 +0300

    hhh

--- firewall_v2.bat
+++ firewall_v2.bat
@@ -198,7 +198,6 @@ exit /b 0
 
 :: error handling
 :error
-set "errorlevel=1"
 echo error: %*
 pause
 cls
@@ -281,13 +280,17 @@ set "selector="
 set "choicelist="
 :: Loop through a list, act on each line
 for /f "eol=L tokens=1" %%a in ('!command!') do (
-    set /a i+=1
-    :: Create dynamic variable names (_1, _2, etc.)
-    for %%b in (_!i!) do (
-        set "%%b=%%a"
-        set "choicelist=!choicelist!!i!"
-        echo !i!. %%a
-    )   )
+	if errorlevel 1 (
+		echo Error: Failed to execute command: !command!
+		endlocal & exit /b 1
+	)
+	set /a i+=1
+	:: Create dynamic variable names (_1, _2, etc.)
+	for %%b in (_!i!) do (
+		set "%%b=%%a"
+		set "choicelist=!choicelist!!i!"
+		echo !i!. %%a
+	)   )
 
 call :reset_choice
 choice /c %choicelist% /n /m "pick option btn %choicelist:~0,1% and %choicelist:~-1,1% ::"
@@ -334,5 +337,3 @@ if "%loop%"=="1" (
 ) else (
     endlocal & exit /b %errorlevel%
 )
-
-endlocal
