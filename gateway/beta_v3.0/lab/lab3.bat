9d27eaedd6eb61422c3fcd1f1c92a2ff370d4731
14ag <muriukipn@gmail.com>
2025-09-11 16:16:34 +0300

    t

--- gateway/beta_v3.0/lab/lab3.bat
+++ gateway/beta_v3.0/lab/lab3.bat
@@ -1,27 +1,29 @@
 ::@echo off
-goto :2
+set "currentDirectory=%~dp0"
+set "foo=%currentDirectory%foo.txt"
+echo %foo%
+pause
 
+
+goto :2
 :2
 set /a "settimeoutx=0"
+echo %settimeoutx%>%foo%
 set /a "settimeouty=3"
+
 call :settimeout 9 echo 9secs
-echo %settimeoutx% XXXXXXXXXXXXXX
-::call :settimeout 8 echo 8secs 
-::call :settimeout 1 echo 1secs
-::call :check_assync
+call :settimeout 8 echo 8secs 
+call :settimeout 1 echo 1secs
+call :check_assync
 echo all done
 pause
 goto :eof
 
 
 :check_assync
-echo "%settimeoutx%"=="%settimeouty%"
+set /p "settimeoutx=<%foo%"
+echo %settimeoutx% __________________________
 if not "%settimeoutx%"=="%settimeouty%" pause & goto :check_assync
-exit /b 0
-
-
-:increment
-set /a "settimeoutx+=1" >nul
 exit /b 0
 
 
@@ -39,9 +41,7 @@ set "first_arg_found=0"
 for /F "tokens=1,* delims= " %%a in ("%args%") do (
     endlocal & ( set "t=%%a" & set "command=%%b")
 )
-::start /b cmd /c "%settimeoutx% >nul 2>&1 & timeout /t %t% /nobreak >nul & %command% & ( set /a "settimeoutx=%1" & set /a "settimeoutx+=1") >nul echo %settimeoutx%"
-set command2=start /b cmd /c "%settimeoutx% ^>nul 2^>^&1 ^& ^( set /a "settimeoutx=%1" ^& set /a "settimeoutx+=1") ^>nul ^& echo %settimeoutx%"
-for /f "tokens=1" %%a in ('%command2%') do set settimeoutx=%%a 
+start /b cmd /v:on /c "timeout /t %t% /nobreak >nul && ( %command% & set /p "x=<%foo%" & set /a "x+=1" & echo %x%>%foo% )"
 echo. >nul
 :eosettimeout
 exit /b
--- gateway/beta_v3.0/lab/lab4.bat
+++ gateway/beta_v3.0/lab/lab4.bat
@@ -1,14 +1,14 @@
 ::@echo off
 set x=0
 
-doskey command2=cmd /v:on /c "%x% ^>nul 2^>^&1 ^& set /a "y=%x%" ^>nul ^& set /a "y+=1" ^>nul ^& echo !y!"
-command2
+echo x>foo.txt
 
-setlocal enabledelayedexpansion
-for /f "tokens=1" %%a in ('command2') do set x=%%a 
-echo %x%_!x!
-pause
-exit /b
+
+
+set /p bar=<foo.txt
+
+echo The variable 'bar' now holds the value: _%bar%_
+
 pause
 :: i want to increase %x% by passing it to a child cmd as the argument %1 then assign it to %x% 
 ::and then retrieve it using echo and reassign using a for /f loop. i must use 'start /b cmd /c'. no temporary files should be created.
