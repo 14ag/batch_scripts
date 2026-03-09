8475d9a82b46565abe226edf967bfd3f16f2b1cd
14ag <muriukipn@gmail.com>
2026-03-09 10:50:28 +0300

    active maintainance

--- delete_temp_files.bat
+++ delete_temp_files.bat
@@ -1,7 +1,4 @@
-@ echo off
+@echo off
 rd %userprofile%\AppData\Local\Temp\ /s/q
-REM if %errorlevel% EQU 0 mkdir %userprofile%\AppData\Local\Temp
-REM if exist %userprofile%\AppData\Local\Temp goto pause
-REM :pause
-REM pause
+rd %windir%\Temp\ /s/q
 exit
